efb9e11269e4c5d8a9e12921acfa658ecef151a9
jnsltk <janos.litkei@gmail.com>
2025-04-06 22:15:42 +0200

    Last commit before ditching bootstrap

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,25 @@
-<nav>
-    <ul>
-        <li>
-            <strong>
-                János's blog
-            </strong>
-        </li>
-    </ul>
-    <ul>
-        <li><a href="about.php">About</a></li>
-        <li><a href="https://janoslitkei.com/" target="_blank">Website</a></li>
-    </ul>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+        <a class="navbar-brand" href="#">János's blog</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarToggler">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://janoslitkei.com/" target="_blank">Portfolio</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
 </nav>
--- public/index.php
+++ public/index.php
@@ -1,11 +1,5 @@
 <?php
-$pageTitle = "Janos's Blog";
-$messages = [];
-$message = "Hello Terezíčku!";
-
-for ($i = 1; $i <= 5; $i++) {
-    $messages[] = strval($i) . ". " . $message;
-}
+$pageTitle = 'János\'s blog';
 ?>
 
 <!DOCTYPE html>
@@ -14,23 +8,18 @@ for ($i = 1; $i <= 5; $i++) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
-
     <title><?= $pageTitle ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="container">
     <?php require_once '../includes/header.php'; ?>
-        <h1>Janos's Blog</h1>
-        <ul>
-            <?php foreach ($messages as $message) : ?>
-                <h2><?= $message ?></h2>
-            <?php endforeach; ?>
-        </ul>
+    <div class="container">
+        <header class="mt-4">
+            <h3>Blog post</h3>
+        </header>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
