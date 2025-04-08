c4a5d806196dc01450a9a3cb0b368a19ca16da1a
jnsltk <janos.litkei@gmail.com>
2025-04-08 09:59:29 +0200

    Set up environment variables, pretty errors

--- public/index.php
+++ public/index.php
@@ -4,11 +4,29 @@
 define('APPROOT', dirname(__DIR__) . '/app/');
 define('VIEWROOT', APPROOT . 'Views/');
 
-// Load composer autoload
+/* --------------------------- Autoload and config -------------------------- */
 require_once __DIR__ . '/../vendor/autoload.php';
-// Load config file
 require_once __DIR__ . '/../app/Config/config.php';
 
+/* ----------------------- Load environment variables ----------------------- */
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->safeLoad();
+
+/* -------------------------- Pretty errors in dev -------------------------- */
+$env = $_ENV['APP_ENV'] ?: 'production';
+
+if ($env == 'development' && class_exists(\Whoops\Run::class)) {
+    $whoops = new \Whoops\Run;
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->register();
+} else {
+    ini_set('display_errors', 0);
+    error_reporting(E_ALL);
+    ini_set('log_errors', 1);
+    ini_set('error_log', __DIR__ . '/../storage/logs/php-error.log');
+}
+
+/* ------------------------------ Bootstrap app ----------------------------- */
 use App\Core\App;
 
 $app = new App();
